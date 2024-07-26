7c72bf9136c3c4189c27eb5d1ecaf1946f1d2936
Antoine Allou Vincheneux <aallou-v@c2r4p6.42lehavre.fr>
2024-07-26 18:25:13 +0200

    Update & Push

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #    By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/20 16:14:26 by aallou-v          #+#    #+#              #
-#    Updated: 2024/07/24 22:59:47 by aallou-v         ###   ########.fr        #
+#    Updated: 2024/07/26 18:24:04 by aallou-v         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,7 @@ from typing import List
 from UserClass import UserEntry, update_user
 from init_var import BOT_TOKEN, GUILD_ID
 import json
+from math import ceil
 
 MY_GUILD = discord.Object(id=GUILD_ID)  # replace with your guild id
 
@@ -41,6 +42,12 @@ def initiation():
 		user_entry.append(UserEntry(entry['login'], entry['heures'], entry['minutes'], entry['secondes'], entry['logtime_total'], entry['pool_year'], entry['pool_month'], entry['pool_level'], entry['level']))
 		print(f"User {entry['login']} added")
 	json_file.close()
+	for entry in user_entry:
+		entry.heures = entry.heures[0]
+		entry.minutes = entry.minutes[0]
+		entry.secondes = entry.secondes[0]
+		entry.logtime_total = entry.logtime_total[0]
+		entry.pool_level = entry.pool_level[0]
 
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
@@ -56,8 +63,6 @@ async def update(interaction: discord.Interaction, login: Optional[str] = None):
 	"""Update des donnees"""
 	if interaction.guild_id != MY_GUILD.id:
 		return await interaction.response.send_message('This command is not available in this guild.', ephemeral=True)
-	if interaction.user.id != 580274372695162891:
-		return await interaction.response.send_message('You are not allowed to use this command.', ephemeral=True)
 	message = 'Commencement de l\'update'
 	if login is not None:
 		message += f' pour {login}'
@@ -73,111 +78,126 @@ async def update(interaction: discord.Interaction, login: Optional[str] = None):
 
 @client.tree.command()
 @app_commands.describe(login='login de la personne dont on veut voir le temps de logtime')
-@app_commands.describe(since='2023-12-30')
-async def logtime(interaction: discord.Interaction, login: Optional[str] = None, since: Optional[str] = None):
-	"""Leaderboard of logtime"""
-	effect = False
-	if login is None and since is None:
-		if interaction.guild_id is None:
-			return await interaction.response.send_message('This command can only be used in a guild.', ephemeral=True)
-		if interaction.guild_id != MY_GUILD.id:
-			return await interaction.response.send_message('This command is not available in this guild.', ephemeral=True)
-		if interaction.user.id != 580274372695162891:
-			effect = True
-		calcul = len(user_entry) / 60
-		number_embed = int(calcul)
-		user_entry.sort(key=lambda x: x.logtime_total, reverse=True)
+async def leaderboard(interaction: discord.Interaction, type: Literal['logtime', 'level', 'piscine-level'], login: Optional[str] = None, pool_year: Optional[str] = None, pool_month: Optional[str] = None):
+	"""Leaderboard of what you want"""
+	if interaction.guild_id is None:
+		return await interaction.response.send_message('This command can only be used in a guild.', ephemeral=True)
+	if interaction.guild_id != MY_GUILD.id:
+		return await interaction.response.send_message('This command is not available in this guild.', ephemeral=True)
+	if type == 'logtime':
+		users: List[UserEntry] = []
+		for entry in user_entry:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
+					continue
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
+					continue
+			users.append(entry)
+		number_of_embed = len(users) / 60
+		number_of_embed = ceil(number_of_embed)
+		if number_of_embed == 0:
+			return await interaction.response.send_message('No data to display', ephemeral=True)
+		embed.clear()
+		users.sort(key=lambda x: x.logtime_total, reverse=True)
 		classement = 1
-		for i in range(number_embed):
-			embed.append(discord.Embed(title=f'Leaderboard Time Page {i+1}/{number_embed}', description='This is a leaderboard of logtime', color=0x00ff00))
+		for i in range(number_of_embed):
+			embed.append(discord.Embed(title=f'Leaderboard Logtime | Page {i + 1}/{number_of_embed}', description='This is a leaderboard of Logtime', color=0x00ff00))
 			embed[i].description = '```'
-			for entry in user_entry[i*60:(i+1)*60]:
-				embed[i].description += f"{classement} | {entry.login} : {entry.heures} heures, {entry.minutes} minutes\n"
-				classement += 1
-			embed[i].description += '```'
-			if effect == True:
-				embed[i].set_footer(text='Vous ne voyez pas votre nom ? C\'est normal, vous n\'êtes pas dans le top 60')
-				await interaction.response.send_message(embed=embed[i], ephemeral=effect)
-				return
-		await interaction.response.send_message(embed=embed[0], ephemeral=effect)
-		for i in range(number_embed):
-			if i != 0:
-				await interaction.channel.send(embed=embed[i])
-	elif login is not None:
-		await interaction.response.send_message(f"Maintenance en cours ....", ephemeral=True)
-	else:
-		await interaction.response.send_message("Command non compris")
-
-@client.tree.command()
-@app_commands.describe(login='login de la personne dont on veut voir le rank')
-@app_commands.describe(month='Mois de la piscine')
-@app_commands.describe(year='Annee de la piscine')
-async def rank(interaction: discord.Interaction, login: Optional[str] = None, month: Literal['july', 'august', 'september', 'all'] = None, year: Literal['2023', '2024', '2025', '2026', 'all'] = None):
-	"""Leaderboard of level"""
-	effect = False
-	if login is None:
-		if interaction.guild_id is None:
-			return await interaction.response.send_message('This command can only be used in a guild.', ephemeral=True)
-		if interaction.guild_id != MY_GUILD.id:
-			return await interaction.response.send_message('This command is not available in this guild.', ephemeral=True)
-		if interaction.user.id != 580274372695162891:
-			effect = True
-		embed = discord.Embed(title='Leaderboard Level Page 1/2', description='This is a leaderboard of Piscine Level', color=0x00ff00)
-		embed2 = discord.Embed(title='Leaderboard Level Page 2/2', description='This is a leaderboard of Piscine Level', color=0xff0000)
-		embed.description = '```'
-		embed2.description = '```'
-		classement = 1
-		user_entry.sort(key=lambda x: x.pool_level, reverse=True)
-		for entry in user_entry:
-			if month == 'all' and year == 'all':
-				if classement <= 60:
-					embed.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				else:
-					embed2.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				classement += 1
-			elif month == 'all' and year != 'all':
-				if entry.pool_year != year:
+		for entry in users:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
 					continue
-				if classement <= 60:
-					embed.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				else:
-					embed2.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				classement += 1
-			elif month != 'all' and year == 'all':
-				if entry.pool_month != month:
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
 					continue
-				if classement <= 60:
-					embed.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				else:
-					embed2.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				classement += 1
+			if login is None:
+				embed[(classement - 1) // 60].description += f"{classement} | {entry.login} : {entry.heures}h{entry.minutes}m\n"
 			else:
-				if entry.pool_month != month or entry.pool_year != year:
-					continue
-				if classement <= 60:
-					embed.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				else:
-					embed2.description += f"{classement} | {entry.login} : {entry.level} level\n"
-				classement += 1
-		embed.description += '```'
-		embed2.description += '```'
-		if effect == True:
-			embed.set_footer(text='Vous ne voyez pas votre nom ? C\'est normal, vous n\'êtes pas dans le top 60')
-		await interaction.response.send_message(embed=embed, ephemeral=effect)
-		if effect == False:
-			await interaction.channel.send(embed=embed2)
-	else:
-		classement = 1
-		find = False
-		for entry in user_entry:
-			if entry.login == login:
-				find = True
-				break
+				if entry.login == login:
+					return await interaction.response.send_message(f"{login} est {classement} dans le classement (logtime : {entry.heures}h{entry.minutes}m)")
 			classement += 1
-		if find == True:
-			return await interaction.response.send_message(f"{login} est {classement} dans le classement (level : {entry.level})")
-		await interaction.response.send_message(f"{login} n'est pas dans le classement")
-		
+		if login is not None:
+			return await interaction.response.send_message(f"{login} n'est pas dans le classement")
+		for i in range(number_of_embed):
+			embed[i].description += '```'
+			await interaction.channel.send(embed=embed[i])
+	elif type == 'level':
+		users: List[UserEntry] = []
+		for entry in user_entry:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
+					continue
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
+					continue
+			users.append(entry)
+		number_of_embed = len(users) / 60
+		number_of_embed = ceil(number_of_embed)
+		if number_of_embed == 0:
+			return await interaction.response.send_message('No data to display', ephemeral=True)
+		embed.clear()
+		users.sort(key=lambda x: x.level, reverse=True)
+		classement = 1
+		for i in range(number_of_embed):
+			embed.append(discord.Embed(title=f'Leaderboard Level | Page {i + 1}/{number_of_embed}', description='This is a leaderboard of Logtime', color=0x00ff00))
+			embed[i].description = '```'
+		for entry in users:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
+					continue
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
+					continue
+			if login is None:
+				embed[(classement - 1) // 60].description += f"{classement} | {entry.login} : {entry.level}\n"
+			else:
+				if entry.login == login:
+					return await interaction.response.send_message(f"{login} est {classement} dans le classement (logtime : {entry.heures}h{entry.minutes}m)")
+			classement += 1
+		if login is not None:
+			return await interaction.response.send_message(f"{login} n'est pas dans le classement")
+		for i in range(number_of_embed):
+			embed[i].description += '```'
+			await interaction.channel.send(embed=embed[i])
+	elif type == 'piscine-level':
+		users: List[UserEntry] = []
+		for entry in user_entry:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
+					continue
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
+					continue
+			users.append(entry)
+		number_of_embed = len(users) / 60
+		number_of_embed = ceil(number_of_embed)
+		if number_of_embed == 0:
+			return await interaction.response.send_message('No data to display', ephemeral=True)
+		embed.clear()
+		users.sort(key=lambda x: x.pool_level, reverse=True)
+		classement = 1
+		for i in range(number_of_embed):
+			embed.append(discord.Embed(title=f'Leaderboard Piscine Level | Page {i + 1}/{number_of_embed}', description='This is a leaderboard of Logtime', color=0x00ff00))
+			embed[i].description = '```'
+		for entry in users:
+			if pool_year is not None:
+				if entry.pool_year[0] != pool_year:
+					continue
+			if pool_month is not None:
+				if entry.pool_month[0] != pool_month:
+					continue
+			if login is None:
+				embed[(classement - 1) // 60].description += f"{classement} | {entry.login} : {entry.pool_level} level\n"
+			else:
+				if entry.login == login:
+					return await interaction.response.send_message(f"{login} est {classement} dans le classement (level : {entry.pool_level})")
+			classement += 1
+		if login is not None:
+			return await interaction.response.send_message(f"{login} n'est pas dans le classement")
+		for i in range(number_of_embed):
+			embed[i].description += '```'
+			await interaction.channel.send(embed=embed[i])
 		
 
 @client.tree.command()
